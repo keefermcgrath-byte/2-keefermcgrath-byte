ebf90f577a6d03c337405bf2e194757f2f9ab5ea
keefermcgrath-byte <keefermcgrath@gmail.com>
2026-01-30 13:52:28 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,13 +1,20 @@
-
 def average_age():
-    # Get User Input
-
+    # Get user input
+    age1 = float(input("Enter the age of friend 1: "))
+    age2 = float(input("Enter the age of friend 2: "))
+    age3 = float(input("Enter the age of friend 3: "))
+    age4 = float(input("Enter the age of friend 4: "))
+    age5 = float(input("Enter the age of friend 5: "))
 
     # Sum ages
+    total_age = age1 + age2 + age3 + age4 + age5
 
     # Average the ages
+    average = total_age / 5
 
     # Print the results
+    print("The average age is:", average)
+
 
 # Line which calls the above function.
 average_age()
